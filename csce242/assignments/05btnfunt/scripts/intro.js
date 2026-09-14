e275735f4e2d075d43d46e57f1cea7088aca6916
alex perez <map39@email.sc.edu>
2026-09-13 23:34:17 -0400

    new assignment

--- csce242/assignments/05btnfunt/scripts/intro.js
+++ csce242/assignments/05btnfunt/scripts/intro.js
@@ -1,42 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    /* Column 1: Speech Bubble */
+    /* Speech */
     const speechCard = document.getElementById("speech-card");
     const speechBubble = document.getElementById("speech-bubble");
-    const toggleSpeechBubble = () => {
+
+    speechCard.addEventListener("click", () => {
         speechBubble.classList.toggle("hidden");
-    };
-
-    speechCard.addEventListener("click", toggleSpeechBubble);
-
-    /* Column 2: Beverage Selection */
-    const beverageSelect = document.getElementById("beverage-select");
-    const beverageResult = document.getElementById("beverage-result");
-    const beverageCard = document.getElementById("beverage-card");
-    beverageCard.addEventListener("click", (event) => {
-        event.stopPropagation();
     });
 
-    const updateBeverageText = (event) => {
+    /* Beverage */
+    const beverageSelect = document.getElementById("beverage-select");
+    const beverageResult = document.getElementById("beverage-result");
+
+    beverageSelect.addEventListener("change", (event) => {
         const chosenBeverage = event.target.value;
         if (chosenBeverage) {
             beverageResult.textContent = `${chosenBeverage}: Nice Choice!`;
         }
-    };
-
-    beverageSelect.addEventListener("change", updateBeverageText);
-
-    /* Column 3: Sticker Placement */
-    const sunImage = document.getElementById("sun-image");
-    const sticker = document.getElementById("sticker");
-    const stickerCard = document.getElementById("sticker-card");
-
-    stickerCard.addEventListener("click", (event) => {
-        event.stopPropagation();
     });
 
-    const addSticker = () => {
-        sticker.classList.remove("hidden");
-    };
+    /* sticker */
+    const sunImage = document.getElementById("sun-image");
+    const sticker = document.getElementById("sticker");
 
-    sunImage.addEventListener("click", addSticker);
+    sunImage.addEventListener("click", (event) => {
+        event.stopPropagation();
+        sticker.classList.remove("hidden");
+    });
 });
